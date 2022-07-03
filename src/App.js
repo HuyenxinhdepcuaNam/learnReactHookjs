fa353d275772a6f532cd35800000bc2f56793fdd
Nam <ptnnamaxe@gmail.com>
2022-07-03 07:33:03 +0700

    video 8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react';
 
 const App = () => {
+  let [name, setName] = useState('Banh Thi Huyen')
+  let [address, setAddress] = useState('')
+  const handleEventClick = () => {
+    setName(address)
+  }
+  const handleOnChangeInput = (event) => {
+    console.log('check', event.target.value)
+    setAddress(event.target.value)
+  }
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h4>Hello world with Banh Thi Huyen</h4>
+        <h4>Hello world with {name}</h4>
+        <input type='text' value={address}
+          onChange={(event) => handleOnChangeInput(event)} />
+        <button type='button' onClick={() => handleEventClick()}>Click here</button>
       </header>
     </div>
   );
